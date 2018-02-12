df24e8d16fd4e60a9b6a70c7d1d367e0d2eea1c0
Wale Jegs <wale.a.jegede@gmail.com>
2018-02-11 22:41:24 -0800

    added logging middleware

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const volleyball = require('volleyball')
 
 const app = express()
 
+app.use(volleyball);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,15 +15,15 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 // app.use('/api', require('./server/api'));
 
-app.get('*', function (req, res, next) {
-  res.sendFile(path.join(__dirname,  '/public/index.html'));
+app.get('*', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.use(function (err, req, res, next) {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
+app.use(function(err, req, res, next) {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
 
-  const PORT = 1993
-  app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
+const PORT = 1993
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
