cceae1041bdaafd897aae0efe3bca14f5d2b135b
Wale Jegs <wale.a.jegede@gmail.com>
2018-02-21 19:08:45 -0500

    frontend changes

--- react/chesschain/src/App.js
+++ react/chesschain/src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
 
   state = {
     master: '',
-    balance: ''
+    balance: '',
+    value: '',
+    message: '',
+    username: ''
   }
 
   async componentDidMount(){
@@ -18,12 +21,51 @@ class App extends Component {
     this.setState({ master, balance })
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await chesschain.methods.newPlayer(this.state.username).send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({message: 'Welcome to ChessChain!'})
+  }
+
   render() {
+    console.log(this.state.username, this.state.value)
     return (
       <div>
-      <h1> ChessChain </h1>
+        <h1> ChessChain </h1>
         <p> Master: {this.state.master}</p>
         <p> Balance: {this.state.balance}</p>
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h3> ChessChain SignUp </h3>
+          <div>
+            <label> Amount of Ether to Send </label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <div>
+            <label> Username </label>
+            <input
+              value={this.state.username}
+              onChange={event => this.setState({ username: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr />
+
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
