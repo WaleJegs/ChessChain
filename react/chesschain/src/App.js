48eb98fbff651b60debc0496f82041b1add04ad1
Wale Jegs <wale.a.jegede@gmail.com>
2018-02-23 17:22:26 -0500

    firebase implemented on frontend

--- react/chesschain/src/App.js
+++ react/chesschain/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import chesschain from './chesschain';
+import axios from 'axios';
+import firebase from './fire';
 
 class App extends Component {
 
@@ -11,14 +13,20 @@ class App extends Component {
     balance: '',
     value: '',
     message: '',
-    username: ''
+    username: '',
+    email: '',
+    password: '',
+    address: '',
+    player: ''
   }
 
   async componentDidMount(){
     const master = await chesschain.methods.master().call();
     const balance = await web3.eth.getBalance(chesschain.options.address)
+    const player = await chesschain.methods.getPlayerInfo(master, 0).call();
+    const accounts = await web3.eth.getAccounts();
 
-    this.setState({ master, balance })
+    this.setState({ master, balance, player, address: accounts[0] })
   }
 
   onSubmit = async (event) => {
@@ -26,6 +34,19 @@ class App extends Component {
     const accounts = await web3.eth.getAccounts();
 
     this.setState({message: 'Waiting on transaction success...'});
+
+    const user = await firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password);
+
+    if (user){
+      console.log('we made it')
+      await firebase.database().ref('users/' + user.V.R).set({
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password,
+        address: [this.state.address],
+        initialValue: this.state.value
+      });
+    }
 
     await chesschain.methods.newPlayer(this.state.username).send({
       from: accounts[0],
@@ -36,12 +57,13 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state.username, this.state.value)
+    console.log(this.state.address)
     return (
       <div>
         <h1> ChessChain </h1>
         <p> Master: {this.state.master}</p>
         <p> Balance: {this.state.balance}</p>
+        <p> Rank: {this.state.player} </p>
 
         <hr />
 
@@ -55,10 +77,28 @@ class App extends Component {
             />
           </div>
           <div>
+            <label> Address </label>
+            <input value={this.state.address}/>
+          </div>
+          <div>
             <label> Username </label>
             <input
               value={this.state.username}
               onChange={event => this.setState({ username: event.target.value })}
+            />
+          </div>
+          <div>
+            <label> Email </label>
+            <input
+              value={this.state.email}
+              onChange={event => this.setState({ email: event.target.value })}
+            />
+          </div>
+          <div>
+            <label> Password </label>
+            <input
+              value={this.state.password}
+              onChange={event => this.setState({ password: event.target.value })}
             />
           </div>
           <button>Enter</button>
---
+++ react/chesschain/src/fire.js
@@ -0,0 +1,12 @@
+const firebase = require('firebase');
+const config = {
+    apiKey: 'AIzaSyDQttoQWqyUPaC5VQmMV2mwniszVzDSdo0',
+    authDomain: 'chesschain.firebaseapp.com',
+    databaseURL: 'https://chesschain.firebaseio.com',
+    projectId: 'chesschain',
+    storageBucket: 'chesschain.appspot.com',
+    messagingSenderId: '1098495793863'
+};
+
+const fire = firebase.initializeApp(config);
+module.exports = fire;
